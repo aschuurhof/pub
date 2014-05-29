e6484b540839cb23fe132acc0ed80daab1865aff
Arno Schuurhof <aschuurhof@gmail.com>
2014-05-29 16:39:06 -0300

    no message

--- json/doubleclick_data_300x50_dfp.js
+++ json/doubleclick_data_300x50_dfp.js
@@ -4,28 +4,18 @@
 "doubleclickAdSize":"300x50",
 "doubleclickIsDfp":true,
 "doubleclickDfpData":
-{
-    "metadata": {
-        "property": "mtv",
-        "hub": "shows",
-        "franchise": "Teen Mom 2",
-        "section": "full-episodes",
-        "title": "A Teen Mom Gets Pregnant.",
-        "is_main": true,
-        "season": 2,
-        "episode": "202"
-    },
-    "mappings": {
-        "ad-unit": {
-            "1": "@property"
-        },
-        "key-values": {
-            "sea": "@season",
-            "ep": "@episode",
-            "foo": "bar"
-        }
-    }
-
-}
+	{
+		"metadata" : {
+			"property" : "mtv_news",
+			"hub" : "breaking",
+			"is_main" : YES
+		},
+			"mappings" : {
+			"ad-unit" : {
+				"1" : "property",
+				"2" : "hub"
+			}
+		}
+	}
 }
 ]
