3fb56c0a500bf05f35cea81024d149ed43a719f1
Arno Schuurhof <arno.schuurhof@redspace.com>
2014-08-22 09:56:31 -0300

    dip data change

--- json/dcDataAds.js
+++ json/dcDataAds.js
@@ -6,23 +6,14 @@
 "doubleclickDfpData":
 {
     "metadata": {
-        "property": "mtv",
-        "hub": "shows",
-        "franchise": "Teen Mom 2",
-        "section": "full-episodes",
-        "title": "A Teen Mom Gets Pregnant.",
-        "is_main": true,
-        "season": 2,
-        "episode": "202"
+        "property": "mtv_news",
+        "hub": "breaking",
+        "is_main": true
     },
     "mappings": {
         "ad-unit": {
             "1": "@property"
-        },
-        "key-values": {
-            "sea": "@season",
-            "ep": "@episode",
-            "foo": "bar"
+            "1": "@hub"
         }
     }
 
