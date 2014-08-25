f82e49caad15006e5f0d73b473fb7275a9af531c
Arno Schuurhof <arno.schuurhof@redspace.com>
2014-08-25 17:45:40 -0300

    no message

--- json/dcDataAds.js
+++ json/dcDataAds.js
@@ -6,15 +6,15 @@
 "doubleclickDfpData":
 {
     "metadata" : {
-        "property"  : "nick",
+        "property"  : "mtv.com",
         "is_main"   : false
     },
     "mappings" : {
         "ad-unit" : {
             "1" : "@property",
-        "2" : "shows",
-        "3" : "victorious",
-        "4" : "home"
+            "2" : "shows",
+            "3" : "victorious",
+            "4" : "home"
         }
     }
 
