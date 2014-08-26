7a91664feb8472c6e83a4f8eb7ccaf9d70f79681
Arno Schuurhof <arno.schuurhof@redspace.com>
2014-08-26 11:58:31 -0300

    no message

---
+++ json/dcDataAdsDart.js
@@ -0,0 +1,23 @@
+[
+{
+"doubleclickKeyValues":"",
+"doubleclickAdSize":"300x50",
+"doubleclickIsDfp":false,
+"doubleclickDfpData":
+{
+    "metadata" : {
+        "property"  : "nick",
+        "is_main"   : false
+    },
+    "mappings" : {
+        "ad-unit" : {
+            "1" : "@property",
+            "2" : "shows",
+            "3" : "victorious",
+            "4" : "home"
+        }
+    }
+
+}
+}
+]
