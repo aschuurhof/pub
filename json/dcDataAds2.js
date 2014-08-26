f13480ac88a454acf012800bdd53a7802c79d24b
Arno Schuurhof <arno.schuurhof@redspace.com>
2014-08-26 13:25:30 -0300

    no message

---
+++ json/dcDataAds2.js
@@ -0,0 +1,23 @@
+[
+{
+"doubleclickKeyValues":"testmode=unilever_1447_300",
+"doubleclickAdSize":"300x250,300x50",
+"doubleclickIsDfp":true,
+"doubleclickDfpData":
+{
+    "metadata" : {
+        "property"  : "nick",
+        "is_main"   : false
+    },
+    "mappings" : {
+        "ad-unit" : {
+            "1" : "@property",
+            "2" : "shows",
+            "3" : "victorious",
+            "4" : "home"
+        }
+    }
+
+}
+}
+]
