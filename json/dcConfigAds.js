05d1c69149cf9d350c95709faf283754d3a15e67
Arno Schuurhof <arno.schuurhof@redspace.com>
2014-08-26 09:26:41 -0300

    no message

--- json/dcConfigAds.js
+++ json/dcConfigAds.js
@@ -17,7 +17,7 @@
 		"omnitureContextData" : {"v.dummyContext1":"1","v.dummyContext2":"2"},
 		"omnitureTimetrackingEnabled" : "true",
 		"doubleclickEnabled":"true",
-		"doubleClickStaticAdAppId":"mtv.com",
+		"doubleClickStaticAdAppId":"btg_test_app",
 		"doubleclickUseExternalBrowser":true,
 		"doubleClickStaticAdIsCoppaCompliant":true,
 		"doubleClickAdIndicatorVisible":true,
