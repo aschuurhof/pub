59628137d77e8c6b002459ff16e642a9ddfe715d
Arno Schuurhof <arno.schuurhof@redspace.com>
2014-10-30 15:24:01 -0300

    iOS ad config

---
+++ ios/test_config.js
@@ -0,0 +1,35 @@
+[
+	{
+		"config" : {
+			"isCoppaCompliant" : false,
+                         "audienceManagerEnabled": true
+
+		},
+		"adData" : {
+			"x" : 0,
+			"y"   : 0,
+			"width" : 300,
+			"height" : 50,
+			"appName" : "btg_test_app",
+			"sections" : "",
+			"isDFP" : false,
+			"enableCloseButton" : true,
+			"closeButtonInterval" : 5,
+			"isRefreshable" : true,
+			"alternativeSizes" : [
+					{
+					"adWidth" : 300,
+					"adHeight" : 50
+					}
+			],
+			"keyValues" : {
+				"!category" : "v1.2.3",
+				"ver"   : "v1.2.3",
+				"testmodePrefix" : "medialets_mraid"
+			},
+			"keychainKeyValues" : {
+				"aamSegments" : ""
+			},
+		}
+	}
+]
