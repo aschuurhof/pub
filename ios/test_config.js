a405a260ab8a3ff53a2516ae06b21b8801a78775
Arno Schuurhof <arno.schuurhof@redspace.com>
2014-10-31 10:20:19 -0300

    no message

--- ios/test_config.js
+++ ios/test_config.js
@@ -8,7 +8,7 @@
 			"y"   : 0,
 			"width" : 300,
 			"height" : 250,
-			"appName" : "btg_test_app",
+			"appName" : "android_test_app",
 			"sections" : "",
 			"isDFP" : false,
 			"enableCloseButton" : true,
