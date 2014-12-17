9899fbed7550f7c98197df3b051083afdca98b7c
Arno Schuurhof <arno.schuurhof@redspace.com>
2014-12-17 11:06:50 -0400

    no message

---
+++ json/dcDataAds_static_size.js
@@ -0,0 +1,24 @@
+[
+{
+"doubleclickKeyValues":"",
+"doubleclickAdSize":"300x250",
+"doubleclickIsDfp":false,
+"doubleclickStaticPixelSize": "575x400",
+"doubleclickDfpData":
+{
+    "metadata" : {
+        "property"  : "nick",
+        "is_main"   : false
+    },
+    "mappings" : {
+        "ad-unit" : {
+            "1" : "@property",
+            "2" : "shows",
+            "3" : "victorious",
+            "4" : "home"
+        }
+    }
+
+}
+}
+]
